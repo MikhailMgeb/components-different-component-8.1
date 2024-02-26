0854e9f536cd666dccc2e65c87dc0e2daf696981
Michael Mgebrishvili <mmd920802@gmail.com>
2024-02-26 16:33:33 +0300

    fire task finish

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import './App.css';
 import { SimpleButton } from './components/Simple-button/Simple-button';
 import { Avatar } from './components/Avatar/Avatar';
 import { users } from './components/Avatar/data-users';
+import { TaskPage } from './components/task-page/Task-page';
+import { tasks } from './components/page-content/data-task-page';
 
 
 
 const App = () => {
   return (
     <div className="App">
-      <SimpleButton text='hello' />
+      <SimpleButton text='click' />
       {
         users.map((user, index) =>
           <Avatar user={user.user} srcPhoto={user.srcPhoto} key={index}/>
         )
       }
+      <TaskPage title={'8.1 Знакомство с React'} tasks={tasks} />
     </div>
   );
 }
